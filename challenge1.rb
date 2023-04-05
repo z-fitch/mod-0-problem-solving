1c8f8da6d55f03ed4ae2605148a508dcc82f22cb
Zanna Fitch <zannafitch2004@gmail.com>
2023-04-05 18:48:33 -0500

    add challenge 1 answer

--- challenge1.rb
+++ challenge1.rb
@@ -2,3 +2,19 @@
 #For each challenge you complete, include comments showing the **overall goal** in your own words, your **pseudocode**, and your **final solution**. 
 
 #Given an array of strings, return only the strings that have exactly 4 characters.
+
+#Check the array for any strings that have 4 characters and return them 
+
+names = ["Zanna", "Gabe", "Sara", "Elena", "Oliver"]
+
+# For each item in the array check if 
+#for .each do |name| 
+#.length of word  ==4 
+#return array
+
+
+names.each do |name|
+    if name.length == 4
+        puts name
+    end
+end
